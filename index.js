d08de25cda4a192462625f2ee24d13e8c11cf5e0
Klimova Alina <alinaklimova82@gmail.com>
2024-08-02 14:37:54 +0600

    Second task. Made it possible to encrypt text using the Vigenère cipher.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const Vigenere = require('caesar-salad').Vigenere;
 const express = require('express');
 
 const app = express();
 const port = 8000;
+const password = 'hello';
 
 app.get('/', (req, res) => {
     return res.send('<h1>Homepage!</h1>');
@@ -9,6 +11,11 @@ app.get('/', (req, res) => {
 
 app.get('/:word', (req, res) => {
     return res.send(`<h1>${req.params.word}</h1>`);
+});
+
+app.get('/encode/:text', (req, res) => {
+  const encodedText = Vigenere.Cipher(password).crypt(req.params.text);
+  return res.send(`<h1>${encodedText}</h1>`);
 });
 
 app.listen(port, () => {
